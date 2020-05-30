067deaf74746d37ee1e79267b6663712dfb91aa7
jaehlee <jaeh.lee@navercorp.com>
2020-05-30 18:58:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## java-test-practice
+### 백기선님의 더 자바, 애플리케이션을 테스트하는 다양한 방법
+ * 강의자료 https://docs.google.com/document/d/1j6mU7Q5gng1mAJZUKUVya4Rs0Jvn5wn_bCUp3rq41nQ/edit
+ * 코드자료 https://github.com/keesun/inflearn-the-java-test
+
---
+++ src/main/java/me/jaehlee/javatestpractice/JavaTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.jaehlee.javatestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestPracticeApplication.class, args);
+    }
+
+}
